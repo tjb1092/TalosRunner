0d86abd96f0fcc11721c8d061fddbae92a458950
Tony Bailey <baileytj@mail.uc.edu>
2018-06-16 13:33:57 -0400

    Forgot these changes. Train now supports the new data struct.

--- train_model.py
+++ train_model.py
@@ -4,15 +4,17 @@ from alexnet import alexnet
 WIDTH = 100
 HEIGHT = 100
 LR = 1e-4
-EPOCHS = 50
-MODEL_NAME = 'pytalos-{}-{}-{}-epocs.model'.format(LR,'alexnet_head_22kSamples',EPOCHS)
+EPOCHS = 100
+Dtype = 'body'
+OPTIMIZER = 'momentum'
+MODEL_NAME = 'pytalos-{}-{}-{}-epocs-{}.model'.format(LR,'alexnet_{}_6kSamples'.format(Dtype),EPOCHS, OPTIMIZER)
 
 model = alexnet(WIDTH,HEIGHT,LR, MODEL_NAME)
 
-train_data = np.load('preprocessedTrainingData/head/training_data_head.npy')
+train_data = np.load('preprocessedTrainingData/{}/training_data_{}.npy'.format(Dtype, Dtype))
 #Automate this to 10% of data bruh
-train = train_data[:-2000]
-test = train_data[-2000:]
+train = train_data[:-600]
+test = train_data[-600:]
 
 X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
 Y = np.array([i[1] for i in train])
